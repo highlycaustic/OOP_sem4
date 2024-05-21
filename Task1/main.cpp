f98a7b1d844bfec28fafad0c3f209267968bb25e
hcl_home <hcl@home>
2024-05-22 00:05:50 +0300

    T1. Проверки функций, настройка include path для заголовков.

--- Task1/main.cpp
+++ Task1/main.cpp
@@ -9,13 +9,20 @@
 
 int main(){
 
+	// Проверка функции расчета длины отрезка
 	assert(triangleSideLength(0, 0, 0, 0) < FLT_EPSILON); // Проверка отрезка в нулевых координатах
 	assert((triangleSideLength(5, 5, -5, 5) - 10.0) < FLT_EPSILON); // Проверка отрезка в I - II четвертях
 	assert((triangleSideLength(7, 9, -11, -13) - 28.42534080710379) < FLT_EPSILON); // Проверка I - III четверти
 
-	assert((trianglePerimeter(10.0, 20.0, 30.0) - 60.0) < FLT_EPSILON);
-	assert((trianglePerimeter(10.123456, 20.987987, 30.456654) - 61.568097) < FLT_EPSILON);
-	//assert((trianglePerimeter(-16.6665, -16.6665, 66.666) - 33.333) < FLT_EPSILON);
+	// Проверка функции расчета периметра
+	assert((trianglePerimeter(10.0, 20.0, 30.0) - 60.0) < FLT_EPSILON); // Ровные значения
+	assert((trianglePerimeter(10.123456, 20.987987, 30.456654) - 61.568097) < FLT_EPSILON); // Значения с частью после запятой
+	assert((trianglePerimeter(-16.666665, -16.666665, 66.66666) - 33.33333) < FLT_EPSILON); // Отрицательные числа
+
+	// Проверка функции расчета площади
+	assert(triangleArea(1.0, 1.0, 0.0) < FLT_EPSILON); // Площадь равна 0 при любой стороне равной 0
+	assert(triangleArea(10.0, 20.0, 30.0) < FLT_EPSILON); // Если сумма двух сторон равна третьей, площадь равна 0
+	assert((triangleArea(42.0, 21.0, 56.0) - 374.577678859) < FLT_EPSILON); // Проверка на обычных значениях
 
 	setlocale(LC_ALL, "Russian");
 

98e9b576b6dce2e0b039582cdc6098ed200fc334
hcl_work <hcl@work>
2024-05-21 17:36:23 +0300

    T1. Проверки (недоделаны)

--- Task1/main.cpp
+++ Task1/main.cpp
@@ -1,7 +1,22 @@
 ﻿#include <iostream>
 #include <trianglecalc.h>
 
+// раскомментировать строку ниже, чтобы отключить assert()
+// #define NDEBUG
+#include <cassert> // для assert
+
+#define _USE_MATH_DEFINES
+
 int main(){
+
+	assert(triangleSideLength(0, 0, 0, 0) < FLT_EPSILON); // Проверка отрезка в нулевых координатах
+	assert((triangleSideLength(5, 5, -5, 5) - 10.0) < FLT_EPSILON); // Проверка отрезка в I - II четвертях
+	assert((triangleSideLength(7, 9, -11, -13) - 28.42534080710379) < FLT_EPSILON); // Проверка I - III четверти
+
+	assert((trianglePerimeter(10.0, 20.0, 30.0) - 60.0) < FLT_EPSILON);
+	assert((trianglePerimeter(10.123456, 20.987987, 30.456654) - 61.568097) < FLT_EPSILON);
+	//assert((trianglePerimeter(-16.6665, -16.6665, 66.666) - 33.333) < FLT_EPSILON);
+
 	setlocale(LC_ALL, "Russian");
 
 	int ax, ay, bx, by, cx, cy;

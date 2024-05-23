4ba17f1dc033d963f040d87abfa50d145429b10b
hcl_home <hcl@home>
2024-05-24 00:19:15 +0300

    T2. Переименование, комментарий, заготовка под функцию-чекер для функций массива

--- Task2/arrmethods.cpp
+++ Task2/arrmethods.cpp
@@ -2,23 +2,25 @@
 #include <iostream>
 #include <random>
 
+// Процедура заполнения массива случайными числами в заданном диапазоне
 void arrRandom(std::vector<int>& arr, int a, int b) {
 	std::random_device rd; // Случайное число, предоставляемое железным ГСЧ
 	std::mt19937 gen(rd()); // Полученное выше число используется в качестве сида для вихря Мерсенна
 	std::uniform_int_distribution<> distr(a, b); // Распределение случайных чисел с ограничением от a до b включительно
-
 
 	for (int i = 0; i < arr.capacity(); ++i) {
 		arr[i] = distr(gen);
 	}
 }
 
+// Процедура заполнения массива факториалами
 void arrFactorial(std::vector<int>& arr) {
 	for (int i = 0; i < arr.capacity(); ++i) {
-		arr[i] = std::tgamma(i + 2); // Используется Гамма-функция, которая при i + 1 равна факториалу
+		arr[i] = std::tgamma(i + 2); // Используется Гамма-функция, которая при i + 1 равна факториалу, и единица сверху для соответсвия индекса в массиве условию задачи
 	}
 }
 
+// Процедура вывода массива
 void outputArray(std::vector<int>& arr) {
 	std::cout << "Элементы массива: ";
 	for (int i = 0; i < arr.capacity(); ++i) {
--- Task2/main.cpp
+++ Task2/main.cpp
@@ -1,13 +1,30 @@
 ﻿#include <iostream>
-#include <arrfunc.h>
+#include <arrmethods.h>
+
+// раскомментировать строку ниже, чтобы отключить assert()
+// #define NDEBUG
+#include <cassert> // для assert
+
+bool checkArrRandom(std::vector<int> test_arr) {
+	return true; // ДОПИСАТЬ
+}
 
 int main() {
+
+	{
+		std::vector<int> test_arr(1);
+		assert(checkArrRandom(test_arr));
+	}
+
 	setlocale(LC_ALL, "Russian");
+
 	int n;
+
 	std::cout << "Введите натуральное число n: ";
 	std::cin >> n;
 
 	std::vector<int> arr(n);
+
 	//arrFactorial(arr);
 	arrRandom(arr, -60, 10);
 	outputArray(arr);

cc2e1d4b1078ff4ff8d2a6beecf070c816429446
hcl_laptop <hcl@laptop>
2024-05-29 09:54:03 +0900

    T3. Комментарии

--- Task3/main.cpp
+++ Task3/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 
+// Выводит поля объекта класса Triangle 
 void printObj(Triangle* obj) {
 	std::cout << "Triangle" << '\n';
 	std::cout << "Side A: " << obj->getSideA() << '\n';
@@ -12,40 +13,41 @@ void printObj(Triangle* obj) {
 }
 
 int main() {
-	Triangle *tri_obj = new Triangle();
+	Triangle *tri_obj = new Triangle(); // Объект с дефолтными значениями для одиночного вывода
 	printObj(tri_obj);
 	std::cout << '\n';
 
-	int arr_size = 3;
+	int arr_size = 3; // Размер массивов далее
 
-	Triangle* tri_arr1 = new Triangle[arr_size];
-	Triangle* tri_arr2 = new Triangle[arr_size];
+	Triangle* tri_arr1 = new Triangle[arr_size]; // Массив для чтения из файла
+	Triangle* tri_arr2 = new Triangle[arr_size]; // Массив для записи
 
-	std::cout << "tri_arr1 ref content:" << '\n';
+	std::cout << "tri_arr1 ref content:" << '\n'; // Вывод объектов первого массива (нужно чтобы увидеть изменения после чтения из файла)
 	for (int i = 0; i < arr_size; ++i) {
 		printObj(&tri_arr1[i]);
 		std::cout << '\n';
 	}
 
-	std::ofstream file_obj;
-	file_obj.open("1.txt", std::ios::app);
+	std::ofstream file_obj; // Файловый объект на вывод
+	file_obj.open("1.txt", std::ios::app); // Создает 1.txt в режиме append. После исполнения программы удалить
 
+	// Цикл записи в файл
 	for (int i = 0; i < arr_size; ++i) {
-		tri_arr2[i].setSideA(1.5);
+		tri_arr2[i].setSideA(1.5); // Задает полям sideA и sideB другие значения, чтобы после чтения были видны изменения
 		tri_arr2[i].setSideB(1.5);
-		file_obj.write((char*)&tri_arr2[i], sizeof(tri_arr2[i]));
+		file_obj.write((char*)&tri_arr2[i], sizeof(tri_arr2[i])); // Записывает в текстовый файл 
 	}
-	file_obj.close();
+	file_obj.close(); // Закрыть файл
 
 	std::cout << "File 1.txt content:" << '\n';
-	std::ifstream file_obj2;
-	file_obj2.open("1.txt", std::ios::in);
+	std::ifstream file_obj2; // Файловый объект на ввод
+	file_obj2.open("1.txt", std::ios::in); // Открыть 1.txt в режиме чтения
 	for (int i = 0; i < arr_size; ++i) {
-		file_obj2.read((char*)&tri_arr1[i], sizeof(tri_arr1[i]));
-		printObj(&tri_arr1[i]);
+		file_obj2.read((char*)&tri_arr1[i], sizeof(tri_arr1[i])); // Записать прочитанное в первый массив
+		printObj(&tri_arr1[i]); // Вывести прочитанное
 		std::cout << '\n';
 	}
-	file_obj2.close();
+	file_obj2.close(); // Закрыть файл
 	return 0;
 
 }
